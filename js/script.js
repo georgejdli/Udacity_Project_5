2b15093bb22f248842907567c7cbf8b5d29182fb
Jay Smith <smith1jason@yahoo.com>
2015-03-21 23:01:09 -0400

    Update
    Added raw Youtube API file, commented out Ajax call for yourMapper2 api

--- js/script.js
+++ js/script.js
@@ -10,17 +10,17 @@ var model = {
       lng:  -85.715553
     },
     {
-      title: "Molly Malone/'s",
+      title: "Molly Malone's",
       lat: 38.241760,
       lng: -85.725012
     },
     {
-      title: "Oshea/'s",
+      title: "Oshea's",
       lat: 38.240664,
       lng: -85.724904
     },
     {
-      title: "Wick/'s Pizza",
+      title: "Wick's Pizza",
       lat: 38.240361,
       lng: -85.724346
     },
@@ -30,7 +30,7 @@ var model = {
       lng:  -85.719583
     },
     {
-      title: "Impellizari/'s",
+      title: "Impellizari's",
       lat: 38.233569,
       lng: -85.711901
     },
@@ -40,7 +40,7 @@ var model = {
       lng: -85.710034
     },
     {
-      title:  "Mark/'s Feed Store BBQ",
+      title:  "Mark's Feed Store BBQ",
       lat: 38.231445,
       lng: -85.708532
     },
@@ -50,7 +50,7 @@ var model = {
       lng: -85.707545
     },
     {
-      title:  "Za'/s Pizzeria",
+      title:  "Za's Pizzeria",
       lat:  38.231209,
       lng:  -85.704734
     },
@@ -65,14 +65,9 @@ var model = {
       lng:  -85.701859
     },
     {
-      title:  "Bard'/s Town Tavern",
+      title:  "Bard's Town Tavern",
       lat:  38.228276,
       lng:  -85.700850
-    },
-    {
-      title:  "Safai Coffee",
-      lat:  38.230038,
-      lng:  -85.705500
     },
     {
       title:  "Cumberland Brewery",
@@ -100,25 +95,10 @@ var model = {
       lng:  -85.714041
     },
     {
-      title:  "Kashmir",
-      lat:  38.236050,
-      lng:  -85.716085
-    },
-    {
-      title: "LaBamba",
-      lat:  38.236850,
-      lng:  -85.717297
-    },
-    {
-      title:  "Heine Bros Coffee",
+      title:  "Heine Brothers Coffee",
       lat:  38.237297,
       lng:  -85.719467
     },
-    {
-      title:  "Homemade Pie Kitchen",
-      lat:  38.239766,
-      lng:  -85.722319
-    }
 ]
 };
 
@@ -179,8 +159,27 @@ var viewModel = function() {
     }
     //console.log(markerArray()
   }();
+
+
+};
+
+ko.applyBindings(new viewModel());
+})();
+
+
+
+
+
+
+
+
+
+
+//Hold onto below for now, just now liking how it works, scant documentation makes it difficult to get it working, going to try Youtube again.
+
+
 //ajax call for yourMapperApp2 from Mashape.com. a dynamic mapping aid app for louisville area
-$.ajax({
+/*$.ajax({
     url: "https://yourmapper2.p.mashape.com/markers?c=1%2C2%2C3&center=1&compact=1&days=30&f=json&id=180&lat=38.234472&lon=-85.713895&num=5&radius=1&search=smith", // The URL to the API. You can get this by clicking on "Show CURL example" from an API profile
     type: 'GET', // The HTTP Method
     data: {}, // Additional parameters here
@@ -191,9 +190,4 @@ $.ajax({
     xhr.setRequestHeader("X-Mashape-Key: C0uqW4lPTtmshIf6Z4RnCTLOzEp8p1QBG2RjsnUH0gWljPn96j"); // Enter here your Mashape key
     }
 });
-
-
-};
-
-ko.applyBindings(new viewModel());
-})();
+*/
