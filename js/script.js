c3f7e9667a9f230e274fd15f6d864a54e94ac48e
smith1jason <smith1jason@users.noreply.github.com>
2015-04-24 21:28:16 -0400

    Update to mirror main branch

--- js/script.js
+++ js/script.js
@@ -1,103 +1,108 @@
-//Anonymous self invoking function=---like this!
-(function() {
+function detectBrowser() {
+  var useragent = navigator.userAgent;
+  var mapdiv = document.getElementById("map-canvas");
 
-var $body = $('body');
-var $player = $('#player');
-var $choice = $('#choice');
+  if (useragent.indexOf('iPhone') != -1 || useragent.indexOf('Android') != -1 ) {
+    mapdiv.style.width = '100%';
+    mapdiv.style.height = '100%';
+  } else {
+    mapdiv.style.width = '600px';
+    mapdiv.style.height = '800px';
+  }
+}
 
-var choice = $choice.val();
-
-var model = {
-            currentMarker: null,
-  markers: [
+var markers = [
     {
       title: "Ramsis on the World",
       lat: 38.235616,
-      lng:  -85.715553
+      lng:  -85.715553,
+      description: "grub"
     },
     {
       title: "Molly Malone's",
       lat: 38.241760,
-      lng: -85.725012
+      lng: -85.725012,
+      description: "pub"
     },
     {
       title: "Oshea's",
       lat: 38.240664,
-      lng: -85.724904
+      lng: -85.724904,
+      description: "pub"
     },
     {
       title: "Wick's Pizza",
       lat: 38.240361,
-      lng: -85.724346
+      lng: -85.724346,
+      description: "grub"
     },
     {
       title: "Nowhere Bar",
       lat: 38.237917,
-      lng:  -85.719583
-    },
-    {
-      title: "Impellizari's",
-      lat: 38.233569,
-      lng: -85.711901
+      lng:  -85.719583,
+      description: "pub"
     },
     {
       title: "Boombozz Pizza",
       lat: 38.231883,
-      lng: -85.710034
+      lng: -85.710034,
+      description: "grub"
     },
     {
       title:  "Mark's Feed Store BBQ",
       lat: 38.231445,
-      lng: -85.708532
+      lng: -85.708532,
+      description: "grub"
     },
     {
       title:  "Seviche",
       lat: 38.231175,
-      lng: -85.707545
+      lng: -85.707545,
+      description: "grub"
     },
     {
       title:  "Cumberland Brewery",
       lat:  38.230729,
-      lng:  -85.705389
+      lng:  -85.705389,
+      description: "pub"
     },
     {
       title:  "Cafe Mimosa",
       lat:  38.231487,
-      lng:  -85.706344
+      lng:  -85.706344,
+      description: "grub"
     },
     {
       title:  "Palermo",
       lat:  38.234069,
-      lng:  -85.712695
+      lng:  -85.712695,
+      description: "grub"
     },
     {
       title:  "Bristol Bar",
       lat:  38.235241,
-      lng:  -85.714041
+      lng:  -85.714041,
+      description: "pub"
     },
     {
       title:  "Heine Brothers Coffee",
       lat:  38.237297,
-      lng:  -85.719467
+      lng:  -85.719467,
+      description: "grub"
     }
-]
-};
+];
 
 var viewModel = function() {
   var map, bounds, markerArray;
-
+  markerArray = [];
   var self = this;
-
-  //Initialize map location, set as IIFE to kick off immediately
+    //Initialize map location, set as IIFE to kick off immediately
   var initMap = function() {
     //create map
     var mapOptions = {
-         center: {
-          //Center map at Cherokee Rd and Bardstown Rd
-            lat: 38.234472,
-            lng: -85.713895
-        },
-        zoom: 15,
+        center: new google.maps.LatLng(38.235616,-85.715553),
+        zoom: 16,
+        mapTypeId: 'terrain',
         panControl: false,
         disableDefaultUI: true
     };
@@ -106,23 +111,28 @@ var viewModel = function() {
 
     bounds = new google.maps.LatLngBounds();
 
+    //create the markerArray for markers to populate on creation
     self.markerArray = ko.observableArray();
-
     //add markers
-    var markerList = model.markers;
-    for(var i = 0; i < markerList.length; i++) {
+    for(var i = 0; i < markers.length; i++) {
       var markPos = new google.maps.LatLng(
-        markerList[i].lat,
-        markerList[i].lng
+        markers[i].lat,
+        markers[i].lng
       );
 //this finally works, leave it alone!
       var marker = new google.maps.Marker({
         position: markPos,
         map: map,
         icon: 'images/marker.png',
-        title: markerList[i].title,
+        title: markers[i].title,
         animation: google.maps.Animation.DROP
       });
+
+      bounds.extend(markPos);
+   //   map.fitBounds(bounds);
+      map.setCenter(bounds.getCenter());
+
+      self.markerArray.push(marker);
 //still need to work on infoWindow, put more content in
 //creates the window for each marker, just applies a generic youtube video at the moment, need to get it working with Ajax to supply video.
 
@@ -141,7 +151,7 @@ var viewModel = function() {
             var ytWindow = new google.maps.InfoWindow({
               content: contentString
               })
-            ytWindow.open(map, marker);
+            ytWindow.open(marker.get('map'), marker);
 
           }
 
@@ -155,4 +165,3 @@ var viewModel = function() {
 };
 
 ko.applyBindings(new viewModel());
-})();
